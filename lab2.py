2ce254a4d4ad0efb06731e6ad9e91d17e8438d66
KhengVatanautdom <156300148+KhengVatanautdom@users.noreply.github.com>
2024-10-26 11:25:18 +0700

    second commit

---
+++ lab2.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import matplotlib.pylab as plt
+filename = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/auto.csv"
+
+
